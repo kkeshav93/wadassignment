7a6b1f4ba5be3ea0e35a2b793303e7aaee69f6ef
Keshav <keshav345@gmail.com>
2016-11-20 18:32:18 -0600

    6: added email to recovery password.

--- recovery.aspx.cs
+++ recovery.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Net;
+using System.Net.Mail;
+using System.Configuration;
+using System.IO;
 
 public partial class recovery : System.Web.UI.Page
 {
@@ -14,7 +18,27 @@ public partial class recovery : System.Web.UI.Page
 
     protected void Button1_Click(object sender, EventArgs e)
     {
-        string script = "alert('Thank You. An email has been sent to the address you have provided');";
-        ClientScript.RegisterClientScriptBlock(this.GetType(), "Alert", script, true);
+        //string script = "alert('Thank You. An email has been sent to the address you have provided');";
+        //ClientScript.RegisterClientScriptBlock(this.GetType(), "Alert", script, true);
+        /* Body of the email is:*/
+        string body_part1 = "Dear Valued Student, <br /> You are receiving this email beause you requested to recover your password.";
+        string body_part2 = "<br /><br />Your current password is:" + "<mark>" + Session["password"] + "</mark>";
+        string body_part3 = "<br /><mark>K.K Student Loan</mark>";
+        string body = body_part1 + body_part2 + body_part3;
+        using (MailMessage mail = new MailMessage(ConfigurationManager.AppSettings["SMTPuser"], TextBox1.Text))
+        {
+            mail.Subject = "We have recovered your password!";
+            mail.Body = body;
+            mail.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = ConfigurationManager.AppSettings["Host"];
+            smtp.EnableSsl = Convert.ToBoolean(ConfigurationManager.AppSettings["EnableSSL"]); ;
+            NetworkCredential NetworkCred = new NetworkCredential(ConfigurationManager.AppSettings["SMTPuser"], ConfigurationManager.AppSettings["SMTPpassword"]);
+            smtp.UseDefaultCredentials = true;
+            smtp.Credentials = NetworkCred;
+            smtp.Port = int.Parse(ConfigurationManager.AppSettings["Port"]); ;
+            smtp.Send(mail);
+            ClientScript.RegisterStartupScript(GetType(), "alert", "alert('Thank You. An email has been sent to the address you have provided');", true);
+        }
     }
 }
